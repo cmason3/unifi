549f35e86f27a07f67058d716e9ae79098e2e351
Chris Mason <57402469+cmason3@users.noreply.github.com>
2023-11-25 09:45:46 +0000

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+exit_handler() {
+  /usr/lib/unifi/bin/unifi.init stop
+  exit ${?};
+}
+
+trap 'kill ${!}; exit_handler' SIGHUP SIGINT SIGQUIT SIGTERM
+
+/usr/lib/unifi/bin/unifi.init start
+wait
+
+exit 1

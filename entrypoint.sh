adc94a3f708d37d1a25828a6c57f5020c08aaa78
Chris Mason <57402469+cmason3@users.noreply.github.com>
2023-11-25 14:09:56 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 
 exit_handler() {
-  echo "in exit handler"
   /usr/lib/unifi/bin/unifi.init stop
-  echo "done exit handler"
   exit ${?}
 }
 
 trap "exit_handler" SIGHUP SIGINT SIGQUIT SIGTERM
 
-echo "About to call start"
 /usr/lib/unifi/bin/unifi.init start
 
 sleep infinity &
 wait ${!}
-echo "dropped out of sleep"
